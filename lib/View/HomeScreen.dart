99a8b1e38fc538ab364587ff9c7d10b8f96a1790
Naresh Mahajan <93301720+naresh2052@users.noreply.github.com>
2023-05-16 11:32:04 +0530

    Update HomeScreen.dart

--- lib/View/HomeScreen.dart
+++ lib/View/HomeScreen.dart
@@ -1,6 +1,7 @@
 
 // ignore_for_file: invalid_use_of_protected_member, unnecessary_null_comparison
 
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
@@ -9,7 +10,7 @@ import '../DataBase/tbldao.dart';
 
 class HomeScreen extends StatefulWidget {
   TblDao tblDao;
-   HomeScreen({Key? key,required this.tblDao}) : super(key: key);
+  HomeScreen({Key? key,required this.tblDao}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -25,57 +26,62 @@ class _HomeScreenState extends State<HomeScreen> {
 
     final size = MediaQuery.of(context).size;
 
-    return  Scaffold(
-      appBar:AppBar(
-        title: const Text('Home Screen'),
-        centerTitle: true,
-      ),
-      body: Obx(() =>
-          getDataController.isLoading.value ?
-           Center(
-            child: Container(
-              height: size.height*0.13,
-                width: size.width*0.13,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.blue,
-                ),
-                child: const Center(
-                  child: SpinKitFadingCircle(
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                )),
-            )
-             :getDataController.hasInternet.value
-              ? ListView.builder(
-              itemCount: getDataController.dataList.length,
-              itemBuilder: (context, index ){
-                return Padding(
-                  padding:  EdgeInsets.only(top: size.height*0.002,left: size.width*0.03,right: size.width*0.03),
-                  child: Card(
-                      child: ListTile(
-                        leading: Container(
-                          width: size.width*0.12,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(image: NetworkImage('${getDataController.dataList[index].owner!.avatarUrl}'))
-                          ),
-                        ),
-                        title: Text('${getDataController.dataList[index].name}'),
-                        subtitle: Text('${getDataController.dataList[index].fullName}'),
-                      )),
-                );
-              }
-          )
-            :buildListView()
-       )
+    return  StreamBuilder<ConnectivityResult>(
+       stream: Connectivity().onConnectivityChanged,
+        builder: (context, snapshot){
+          return Scaffold(
+              appBar:AppBar(
+                title: const Text('Home Screen'),
+                centerTitle: true,
+              ),
+              body: Obx(() =>
+              getDataController.isLoading.value ?
+              Center(
+                child: Container(
+                    height: size.height*0.13,
+                    width: size.width*0.13,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue,
+                    ),
+                    child: const Center(
+                      child: SpinKitFadingCircle(
+                        color: Colors.white,
+                        size: 50.0,
+                      ),
+                    )),
+              )
+                  :snapshot.data != ConnectivityResult.none
+                  ? ListView.builder(
+                  itemCount: getDataController.dataList.length,
+                  itemBuilder: (context, index ){
+                    return Padding(
+                      padding:  EdgeInsets.only(top: size.height*0.002,left: size.width*0.03,right: size.width*0.03),
+                      child: Card(
+                          child: ListTile(
+                            leading: Container(
+                              width: size.width*0.12,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(image: NetworkImage('${getDataController.dataList[index].owner!.avatarUrl}'))
+                              ),
+                            ),
+                            title: Text('${getDataController.dataList[index].name}'),
+                            subtitle: Text('${getDataController.dataList[index].fullName}'),
+                          )),
+                    );
+                  }
+              )
+                  :buildListView()
+              )
+          );
+        }
     );
   }
 
   @override
   void initState() {
-   getDataController.getData(context,widget.tblDao);
+    getDataController.getData(context,widget.tblDao);
     super.initState();
   }
 
@@ -84,7 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget buildListView(){
     final size = MediaQuery.of(context).size;
     return FutureBuilder(
-      future: widget.tblDao.getAllData(),
+        future: widget.tblDao.getAllData(),
         builder: (index,snapshot){
           if (snapshot.data == null) {
             return Center(
