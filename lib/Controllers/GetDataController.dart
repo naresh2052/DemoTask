f8ce0d18a152aa6f90ad67b157d62b2cf91acb28
Naresh Mahajan <93301720+naresh2052@users.noreply.github.com>
2023-05-16 11:28:52 +0530

    Update GetDataController.dart
    Third Commit

--- lib/Controllers/GetDataController.dart
+++ lib/Controllers/GetDataController.dart
@@ -1,11 +1,9 @@
-// ignore_for_file: invalid_use_of_protected_member, use_build_context_synchronously
+// ignore_for_file: invalid_use_of_protected_member, use_build_context_synchronously, unnecessary_null_comparison
 
 import 'dart:developer';
-import 'package:demotask/Utils/Show_Dailog.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import '../DataBase/entity.dart';
 import '../DataBase/tbldao.dart';
 import '../Model/DataModel.dart';
@@ -14,13 +12,11 @@ import '../Utils/AppConstants.dart';
 class GetDataController extends GetxController{
 
   var isLoading = false.obs;
-  var hasInternet = false.obs;
+  TblDao? tblDao;
   RxList<DataModel> dataList= <DataModel>[].obs;
-
-  Future getData(BuildContext context,TblDao? tblDao)async{
-    hasInternet.value = await InternetConnectionChecker().hasConnection;
+  Future getData(BuildContext context, TblDao? tblDao)async{
    try{
-     if(hasInternet.value) {
+
        isLoading.value = true;
        final dio = Dio();
        var response = await dio.get(AppConstants.url);
@@ -28,23 +24,18 @@ class GetDataController extends GetxController{
        if (response.statusCode == 200) {
          for (var i in response.data) {
            dataList.value.add(DataModel.fromJson(i));
-               tblDao!.insertData(
-                   TblData(
-                     name:DataModel.fromJson(i).name!,
-                     fullName: DataModel.fromJson(i).fullName!,
-                     avatarUrl: DataModel.fromJson(i).owner!.avatarUrl!,
-                   )
-               );
+             tblDao!.insertData(
+                 TblData(
+                   name:DataModel.fromJson(i).name!,
+                   fullName: DataModel.fromJson(i).fullName!,
+                   avatarUrl: DataModel.fromJson(i).owner!.avatarUrl!,
+                 )
+             );
          }
          isLoading.value = false;
        }
-     }else{
-      context.showMessageDialog("No Internet Connection!!");
-
-     }
    }catch(e){
      print(e.toString());
    }
   }
-
 }
